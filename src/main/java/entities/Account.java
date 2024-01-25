3eeffa8c4900896322c212f19e489ac8d535fef2
adu <aluta182004@gmail.com>
2024-01-25 21:41:18 +0000

    implemented accounts

--- src/main/java/entities/Account.java
+++ src/main/java/entities/Account.java
@@ -1,4 +1,15 @@
 package entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "accounts")
 public class Account {
+    @Id
+    private int id;
+    private String name;
+    private String createdAt;
+    //whether an expense, income or investment account
+    private String accountType;
+
 }

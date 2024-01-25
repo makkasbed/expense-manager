722e06b664c29dae4315769dc61433cf1643e0ee
adu <aluta182004@gmail.com>
2024-01-25 21:46:07 +0000

    implemented transaction

--- src/main/java/entities/Transaction.java
+++ src/main/java/entities/Transaction.java
@@ -1,4 +1,16 @@
 package entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name = "transactions")
 public class Transaction {
+    @Id
+    private int id;
+    private double amount;
+    private String transactionDate;
+    private String debitAccount;
+    private String creditAccount;
+    private String narration;
+    private String status;
 }
